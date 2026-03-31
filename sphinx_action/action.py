dfe140df312b7bb74c61b29bae617357c639f457
runora95 <runor.agbaire@xanadu.ai>
2026-03-31 13:19:10 -0400

    Changing docs requirements file to use dependency group

--- sphinx_action/action.py
+++ sphinx_action/action.py
@@ -99,9 +99,9 @@ def build_docs(build_command, docs_directory):
     if not build_command:
         raise ValueError("Build command may not be empty")
 
-    docs_requirements = os.path.join(docs_directory, "requirements.txt")
+    docs_requirements = os.path.join(docs_directory, "pyproject.toml")
     if os.path.exists(docs_requirements):
-        subprocess.check_call(["pip", "install", "-r", docs_requirements])
+        subprocess.check_call(["pip", "install", "--group", "docs"])
 
     log_file = os.path.join(tempfile.gettempdir(), "sphinx-log")
     if os.path.exists(log_file):
